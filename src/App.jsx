9e915ddb0949bf857a5f679873bc0cd205424e68
wanguinuthu <wanguinuthu5@gmail.com>
2022-11-18 15:15:05 +0300

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,18 @@ function App() {
   return(
     <div className="App">
       <Navbar>Chat-App</Navbar>
-      <Card/>
+      <Card
+      profile_img='src/assets/girl.jpeg'
+      chat_name='Kui'
+      chat_time='2:00pm'
+      chat_content='Hello'
+      chat_number='12'/>
+      <Card
+      profile_img='src/assets/girl.jpeg'
+      chat_name='Kui'
+      chat_time='2:00pm'
+      chat_content='Hello'
+      chat_number='12'/>
     </div>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,20 +3,20 @@ import Source from '../assets/source.jpeg'
 
 import React from 'react'
 
-function Card() {
+function Card(props) {
   return (
     <div className='container'>
 <div className="profile-img">
-   <img id='profile-pic' src={Source}/>
+   <img id='profile-pic' src={props.profile_img}/>
 </div>
 <div className="content">
     <div className="top-content">
-<h5>Femi-Dev</h5>
-<h5>9:20pm</h5>
+<h5 id='chat-name'>{props.chat_name}</h5>
+<h5 id='last-seen'>{props.chat_time}</h5>
     </div>
     <div className="bottom-content">
-<p>Hey, how are you?</p>
-<h6>12</h6>
+<p id='chat-content'>{props.chat_content}</p>
+<h6 id='chat-number'>{props.chat_number}</h6>
     </div>
 </div>
     </div>
